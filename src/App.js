6893f3a377c596d94ed152d31149322d0d9d5352
Otarichi <gvazava911@gmail.com>
2021-08-04 17:30:26 +0400

    firstCommit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Welcome_page from './Components/welcome_auth_comps/welcome_page';
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome_page></Welcome_page>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/welcome_auth_comps/welcome_page.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const welcome_page = () => {
+    return (
+        <div>
+            <div class="logo logo-white">
+                <p>აბადელი</p>
+            </div>
+
+            <div class="text-typea-white">
+                <p>მოგესალმებით აბადელიზე, შეუკვეთე შენთვის სასურველი პროდუქტი, დაზოგე დრო და იყავი ბედნიერი, აბადელისთან ერთად</p>
+            </div>
+
+            <div class="large-button-white">
+                
+            </div>
+        </div>
+    );
+}
+
+export default welcome_page;
